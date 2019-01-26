16a6d38e1fb50c8285a5946070d5c00c12710429
CristianoPrivitera <cristiano.privitera@live.com>
2019-01-26 11:26:02 +0100

    fix Mappa Placeholder

--- data/Textures/Tileset/Pavimento.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Pavimento" tilewidth="128" tileheight="128" tilecount="1" columns="1">
- <image source="../Test Block.png" width="128" height="128"/>
-</tileset>
