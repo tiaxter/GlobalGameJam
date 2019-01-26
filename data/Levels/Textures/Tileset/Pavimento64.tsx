cafce53faf95f34e1446c42b43fef7c5e708ffee
CristianoPrivitera <cristiano.privitera@live.com>
2019-01-26 13:23:50 +0100

    update MappaPlaceholder e GDD

--- data/Levels/Textures/Tileset/Pavimento64.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Pavimento64" tilewidth="64" tileheight="64" tilecount="9" columns="3">
- <image source="../Tiles/Floor 5x5.png" width="192" height="192"/>
-</tileset>
--- data/Levels/Textures/Tileset/TestPavimento.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="TestPavimento" tilewidth="128" tileheight="128" tilecount="1" columns="1">
- <image source="../Tiles/Test Block.png" width="128" height="128"/>
-</tileset>
