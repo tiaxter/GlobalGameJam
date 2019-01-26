fd068db24d8f5c1f8351e2457e9037d819d858ac
CristianoPrivitera <cristiano.privitera@live.com>
2019-01-26 19:24:39 +0100

    test mappa

--- data/Levels/Placeholder/Walkable.tsx
+++ data/Levels/Placeholder/Walkable.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Walkable" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+<tileset version="1.2" tiledversion="1.2.1" name="Walkable" tilewidth="192" tileheight="192" tilecount="1" columns="1">
  <image source="Walkable.png" width="64" height="64"/>
 </tileset>
